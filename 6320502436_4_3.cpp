7f56dfa74b8df47248e41d02f8e6542920f35b85
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-04 16:51:24 +0800

    Create 6320502436_4_3.cpp

---
+++ 6320502436_4_3.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	int a,b=0,c,d,e=1,g;
+	char A[10000]={},B[1000]={},f;
+	scanf("%s",A);
+	g=strlen(A);
+	do
+	{
+		for(a=0;a<=g;a++)
+		{
+			if(B[b]!=A[a])
+			{
+				B[b+1]=A[a];
+				//printf("%c-%d ",B[b+1],b+1);
+				b++;
+			}
+			
+		}
+		
+	}while(a<g);
+	for(a=0;a<=b;a++){
+	printf("%c",B[a+1]);}
+}
