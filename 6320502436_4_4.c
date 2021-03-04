ea3ffbb507c1a1068cdc2388ab66c827088cc198
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-04 18:14:02 +0800

    Update 6320502436_4_4.c

--- 6320502436_4_4.c
+++ 6320502436_4_4.c
@@ -1,15 +1,29 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 int main()
 {
-	int  a,b,c,d,e;
-	char A[1000]={};
-	a=strlen[A];
-	for(b=0;b<=a;b++)
-	{
-		A[b]=
-	}
-	
-	
-	
+    int i=0;
+    char B[1000];
+    scanf("%[^\n]",B);
+    char* A=strtok(B," ");
+    while(A!=NULL)
+    {
+        if(i==0)
+        {
+            printf("%c",toupper(A[0]));
+        }
+        else
+        {
+
+            if(strcmp(A,"i")!=0&&strcmp(A,"of")!=0&&strcmp(A,"the")!=0&&strcmp(A,"on")!=0&&strcmp(A,"at")!=0&&strcmp(A,"for")!=0
+                &&strcmp(A,"with")!=0&&strcmp(A,"a")!=0&&strcmp(A,"an")!=0&&strcmp(A,"in")!=0&&strcmp(A,"and")!=0)
+            {
+                printf("%c",toupper(A[0]));
+            }
+        }
+        i=1;
+        A=strtok(NULL," ");
+    }
+
 }
