80815da728edccc6199d02d63593e1c1b2afdf25
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-04 16:36:27 +0800

    Update 6320502436_4_1.cpp

--- 6320502436_4_1.cpp
+++ 6320502436_4_1.cpp
@@ -1,25 +1,25 @@
 #include<stdio.h>
+#include<string.h>
 main()
 {
-	int a,b=0,c,d,e=1;
+	int a,b=0,c,d,e=1,g;
 	char A[100]={},B[10]={},f;
 	scanf("%s",A);
+	g=strlen(A);
 	do
 	{
-		for(a=0;A[a]!=' ';a++)
+		for(a=0;a<=g;a++)
 		{
 			if(B[b]!=A[a])
 			{
 				B[b+1]=A[a];
-				printf("%c-%d ",B[b+1],b+1);
+				//printf("%c-%d ",B[b+1],b+1);
 				b++;
 			}
-			if(A[a+1]==' ')
-			{e=0;
-			break;}
+			
 		}
 		
-	}while(A[a+1]==' ');
-	for(a=1;a<b;a++){
+	}while(a<g);
+	for(a=0;a<=b;a++){
 	printf("%c",B[a+1]);}
 }
